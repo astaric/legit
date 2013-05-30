8fb83c3438193b44fb1e4f8f413c98c95a4511e5
astaric <anze.staric@gmail.com>
2013-05-30 19:22:28 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Legit',
+    version='0.1',
+    description='Legacy git',
+    author='Anze Staric',
+    author_email='anze@staric.net',
+    url='http://github.com/astaric/legit',
+    scripts=['import_to_git'],
+)
